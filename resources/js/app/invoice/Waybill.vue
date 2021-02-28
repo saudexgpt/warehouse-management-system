c2ba2d23cc5ff3e1d194f8cf3003843318ec10ec
sammy4best <saudexgpt@gmail.com>
2021-03-01 00:23:18 +0100

    New update on waybill modification

--- resources/js/app/invoice/Waybill.vue
+++ resources/js/app/invoice/Waybill.vue
@@ -79,7 +79,7 @@
           </div>
           <div slot="action" slot-scope="props">
             <a class="btn btn-default" @click="waybill=props.row; page.option='waybill_details'"><i class="el-icon-tickets" /></a>
-            <a v-if="checkPermission(['generate waybill']) && props.row.status==='pending'" class="btn btn-warning" @click="waybill=props.row; page.option='edit_waybill'"><i class="el-icon-edit" /></a>
+            <a v-if="checkPermission(['generate waybill']) && props.row.confirmed_by===null" class="btn btn-warning" @click="waybill=props.row; page.option='edit_waybill'"><i class="el-icon-edit" /></a>
             <a v-if="props.row.dispatch_products.length < 1 && checkPermission(['delete pending waybill'])" class="btn btn-danger" @click="deleteWaybill(props.index, props.row)"><i class="el-icon-delete" /></a>
 
             <a v-if="props.row.status === 'waybill_generated'" class="btn btn-default" @click="waybill=props.row; waybill.index= props.index - 1; page.option='waybill_details'"><i class="el-icon-success" /></a>
